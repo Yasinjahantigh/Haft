4eccb2c693698e263a91298f39efe4d512e01022
arminatinad <armindaliri87@gmail.com>
2024-06-29 18:44:57 +0330

    camera

--- Assets/Scripts/camera.cs
+++ Assets/Scripts/camera.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 public class camera : MonoBehaviour
 {
     public Transform characterTransform;
-    
-    void Start()
-    {
-        
-    }
+
+    //public float offesty;
+    //float lastposx;
+    //public Transform farbackground, middlebackground;
+    //private void Start()
+    //{
+    //    lastposx = transform.position.x;    
+    //}
 
     void Update()
     {
-        transform.position = new Vector3(characterTransform.position.x, characterTransform.position.y, transform.position.z);
+        transform.position = new Vector3(characterTransform.position.x,transform.position.y, transform.position.z);
+
+        //float amounttomovex = transform.position.x - lastposx;
+        //farbackground.position = farbackground.position + new Vector3(amounttomovex, 0f, 0f);
+        //middlebackground.position += new Vector3(amounttomovex * 0.5f, 0f, 0f);
+        //lastposx = transform.position.x;
     }
 }
 

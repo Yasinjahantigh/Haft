c57ad1aba5be671bcf37ea30bbcb0a509df5a7ae
arminatinad <armindaliri87@gmail.com>
2024-07-25 20:20:19 +0330

    درست کردن زمان تیر
    تیر و خنجر بعد ز چند ثانیه از بین می رن

--- Assets/Scripts/rostam_code.cs
+++ Assets/Scripts/rostam_code.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 //using UnityEngine.UIElements;
-using UnityEngine.UI;
 using System.IO;
 using UnityEngine.SceneManagement;
 using TMPro;
